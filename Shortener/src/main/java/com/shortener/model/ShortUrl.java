54ae2466d844c320952154cb30a7be4cba862b54
Fernando Roocha Gouvêa <fernandosrg1@gmail.com>
2017-12-16 19:41:00 -0200

    Criando modelo e repositório para acesso de dados.

---
+++ Shortener/src/main/java/com/shortener/model/ShortUrl.java
@@ -0,0 +1,45 @@
+package com.shortener.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+@Entity
+public class ShortUrl {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String url;
+	
+	@Column(nullable = false)
+	private String alias;
+	
+	public Integer getId() {
+		return this.id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getUrl() {
+		return this.url;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public String getAlias() {
+		return this.alias;
+	}
+	
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+}
---
+++ Shortener/src/main/java/com/shortener/repository/ShortUrlRepository.java
@@ -0,0 +1,8 @@
+package com.shortener.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.shortener.model.ShortUrl;
+
+public interface ShortUrlRepository extends CrudRepository<ShortUrl, Long> {
+	
+}
