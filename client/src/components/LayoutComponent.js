24a0d07a0b1d024573d7d72b3fc5c4e9691f1672
Fernando Gouvea <fernandosrg1@gmail.com>
2017-12-18 22:48:07 -0200

    Cliente: Adicionando top 10.

--- client/src/components/LayoutComponent.js
+++ client/src/components/LayoutComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Grid, Col, Panel } from 'react-bootstrap';
+import { Navbar, Grid, Row, Col, Panel } from 'react-bootstrap';
 import '../css/bootstrap-theme.css';
 
 import ShortenUrlComponent from './ShortenUrlComponent';
 import ErrorComponent from './ErrorComponent';
+import Top10Component from './Top10Component';
 
 class LayoutComponent extends Component {
 
@@ -35,13 +36,23 @@ class LayoutComponent extends Component {
         </header>
 
         <Grid>
-          <Col md={8} mdOffset={2} sm={12}>
+          <Row>
+            <Col md={8} mdOffset={2} sm={12}>
             <Panel header={this.getPanelTitle()} >
             {
               this.getContent()
             }
             </Panel>
-          </Col>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={8} mdOffset={2} sm={12}>
+            <Panel header="Most Viewed" >
+              <Top10Component />
+            </Panel>
+            </Col>
+          </Row>
         </Grid>
       </div>
     );
---
+++ client/src/components/Top10Component.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import { Table } from 'react-bootstrap';
+
+import ErrorComponent from './ErrorComponent';
+
+class Top10Component extends Component {
+
+  constructor() {
+    super();
+    this.state = { }
+  }
+  componentWillMount() {
+    axios.get('/top10Urls')
+         .then(this.handleAPIResponse.bind(this))
+         .catch(this.handleError.bind(this));
+  }
+
+  handleAPIResponse(response) {
+    this.setState({
+      top10: response.data
+    });
+  }
+
+  handleError(error) {
+    const top10_error = {err_code: '000', description: 'Failed to load top 10 urls.'}
+    this.setState({
+      error: top10_error
+    });
+  }
+
+  getUrlRow(url, index) {
+    return (
+      <tr key={'url' + index}>
+        <th>{index + 1}</th>
+        <th><a href={url.url}>{window.location.href + url.alias}</a></th>
+        <th>{url.url}</th>
+        <th>{url.views}</th>
+      </tr>
+    );
+  }
+
+  getTop10Table() {
+    return (
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Short</th>
+            <th>Full</th>
+            <th>Views</th>
+          </tr>
+          </thead>
+
+          <tbody>
+            {
+              this.state.top10.map((url, index) => this.getUrlRow(url, index))
+            }
+          </tbody>
+      </Table>
+    );
+  }
+
+  render() {
+    if (this.state.error) {
+      return <ErrorComponent error={this.state.error} />
+    } else if (this.state.top10) {
+      return this.getTop10Table()
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Top10Component;
